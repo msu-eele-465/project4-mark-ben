a4563d8a854c238985815bbecb46bd3cc3152889
Mark-H-W <kk7byr@wildmile.com>
2025-03-11 12:16:47 -0600

    initial lcd code

--- i2c-lcd/app/main.c
+++ i2c-lcd/app/main.c
@@ -1,20 +1,108 @@
+#include "intrinsics.h"
 #include <msp430fr2310.h>
 #include <stdbool.h>
+
+void lcd_raw_send(int send_data, int num) {
+    int send_data_temp;
+    int nibble;
+    int i = 0;
+    int busy = 1;
+    send_data_temp = send_data;
+
+    while (i < num) {
+
+        // Get top nibble
+        if (num == 2) {
+            nibble = send_data_temp & 0b11110000;
+            send_data_temp = send_data_temp << 4;
+        } else {
+            nibble = send_data_temp & 0b00001111;
+            send_data_temp = send_data_temp >> 4;
+            nibble = nibble << 4;
+        }
+   
+        // Output nibble
+        P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+        P1OUT |= nibble;
+
+        // Set enable high
+        P1OUT |= BIT1;
+        _delay_cycles(100);
+
+        // Wait and drop enable
+        _delay_cycles(100);
+        P1OUT &= ~BIT1;
+        _delay_cycles(100);
+
+        i++;
+    }
+    
+    P1DIR &= ~(BIT4 | BIT5 | BIT6 | BIT7); // Set input
+    P1OUT |= BIT0;
+    P2OUT &= ~BIT6;
+
+    // Check busy flag
+    while (busy != 0) {
+        _delay_cycles(1000);
+        P1OUT |= BIT1;       // Enable high
+        _delay_cycles(100);
+
+        busy = P1IN & BIT7;  // Read busy
+
+        _delay_cycles(100);
+        P1OUT &= ~BIT1;     // Enable low
+
+        _delay_cycles(100);
+        P1OUT |= BIT1;     // Enable high
+        _delay_cycles(200);
+        P1OUT &= ~BIT1;    // Enable low
+    }
+
+    P1DIR |= (BIT4 | BIT5 | BIT6 | BIT7);  // Set output
+    P1OUT &= ~BIT0;
+}
+
+
+void lcd_string_write(char* string, int n) {
+    int i = 0;
+
+    while (i < n) {
+        P2OUT |= BIT6;
+        lcd_raw_send((int)string[i], 2);
+        i++;
+    }
+}
 
 int main(void)
 {
     // Stop watchdog timer
+    
     WDTCTL = WDTPW | WDTHOLD;
 
-    P1OUT &= ~BIT0;
-    P1DIR |= BIT0;
+    P2OUT &= ~BIT0;
+    P2DIR |= BIT0;
+
+    P1OUT &= ~BIT0; // P1.1 is R/W
+    P1DIR |= (BIT0 | BIT1 | BIT4 | BIT5 | BIT6 | BIT7);
+
+    P2OUT &= ~BIT6; // RS PIN
+    P2DIR |= BIT6;
 
     // Disable low-power mode / GPIO high-impedance
     PM5CTL0 &= ~LOCKLPM5;
 
+
+    // Init LCD
+    lcd_raw_send(0b110000100010, 3); // Turn on LCD in 2-line mode
+    lcd_raw_send(0b00001100, 2); // Display on, cursor off, blink off
+    lcd_raw_send(0b00000001, 2); // Clear display
+    lcd_raw_send(0b00000110, 2); // Increment mode, entire shift off
+
+    lcd_string_write("Hello! :)", 9);
+
     while (true)
     {
-        P1OUT ^= BIT0;
+        P2OUT ^= BIT0;
 
         // Delay for 100000*(1/MCLK)=0.1s
         __delay_cycles(100000);
